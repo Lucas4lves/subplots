825f8cb564f4a5715181c76b14876e085852570e
Lucas4lves <zombnfl@gmail.com>
2025-11-15 01:11:57 -0300

    feature(backend): postgres driver config

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/Lucas4lves/subplots/database"
+	"github.com/Lucas4lves/subplots/internal/container"
 	"github.com/Lucas4lves/subplots/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -8,6 +10,12 @@ import (
 func main() {
 	server := gin.Default()
 	env := utils.LoadEnv()
+
+	dbCfg := database.NewDatabaseConfig("postgres", env.DSN)
+
+	defer dbCfg.Driver.Close()
+
+	dc := container.NewDependencyContainer(dbCfg.Driver)
 
 	server.Run("0.0.0.0:" + env.PORT)
 }
--- backend/utils/environment.go
+++ backend/utils/environment.go
@@ -4,10 +4,12 @@ import "os"
 
 type Environent struct {
 	PORT string
+	DSN  string
 }
 
 func LoadEnv() *Environent {
 	return &Environent{
 		PORT: os.Getenv("SERVICE_PORT"),
+		DSN:  os.Getenv("DSN"),
 	}
 }
